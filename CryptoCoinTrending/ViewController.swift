36c5b8f331f6565e67122e2c062aca35b13eb423
hwiwls <80039139+hwiwls@users.noreply.github.com>
2024-02-27 14:58:20 +0900

    Initial Commit

---
+++ CryptoCoinTrending/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoCoinTrending
+//
+//  Created by hwijinjeong on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
